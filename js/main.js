0d4f8d28e70fb5993414cad879aac4ac19496e7a
KyleRattet <krattet@gmail.com>
2015-08-04 11:32:36 -0600

    added exercises work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
-// add scripts
+// *** Intro to OOP *** //
 
-console.log("sanity check!");
+// Base Class - constructor
+// var Vehicle = function(numberOfWheels, carColor){
+//   // properties
+//   this.wheels = numberOfWheels;
+//   this.color = carColor;
+// };
+
+// //Methods
+// Vehicle.prototype.honk = function () {
+//   return 'honk!';
+// };
+
+// Vehicle.prototype.toString = function () {
+//   return 'The vehilce has ' + this.wheels + ' wheels and is ' + this.color + '.';
+// };
+
+// // Instances
+// var redCar = new Vehicle(4, 'red');
+// console.log(redCar);
+// console.log(redCar.honk());
+// console.log(redCar.toString());
+
+//Base Class - constructor
+
+var Song = function (songName, bandName) {
+  this.song = songName;
+  this.band = bandName;
+
+};
+
+//Methods
+Song.prototype.countName = function () {
+  return this.song.split(" ").length
+};
+
+Song.prototype.createElement = function () {
+  var newElement = $('<p></p>');
+  newElement.text(this.song + ' by ' + this.band);
+  return newElement;
+};
+
+
+//Instances
+var newSong = new Song('A Day in the Life', 'The Beatles');
+console.log(newSong);
+console.log(newSong.countName());
+
+$( document ).ready(function() {
+    $('body').append(newSong.createElement());
+});
